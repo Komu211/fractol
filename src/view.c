25809f8d644b7b514853493dbccdcee3a5a43a13
Komu211 <75761278+Komu211@users.noreply.github.com>
2024-11-25 01:06:08 +0100

    Working prototype (no scrolling, no moving, no colors)

--- src/view.c
+++ src/view.c
@@ -6,7 +6,7 @@
 /*   By: kmuhlbau <kmuhlbau@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/24 17:58:36 by kmuhlbau          #+#    #+#             */
-/*   Updated: 2024/11/24 23:47:28 by kmuhlbau         ###   ########.fr       */
+/*   Updated: 2024/11/25 01:05:13 by kmuhlbau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,7 @@ void	render_fractal(t_fract_data *fract_data)
 			//	color = get_julia_color(x, y);
 			// else if (fract_data->type == FRACTAL_BURNING_SHIP)
 			//	color = get_burning_ship_color(x, y);
-			mlx_pixel_put(fract_data->img, x, y, color);
+			mlx_put_pixel(fract_data->img, x, y, color);
 			x++;
 		}
 		y++;
